48d87abd55a397a953b38b67db8f49db07c3c05e
lilianbrinzan <lilianbrinzan@gmail.com>
2023-08-30 17:54:31 +0200

    first commit

---
+++ backend-demo/src/main/java/de/ait/todo/services/TasksService.java
@@ -0,0 +1,21 @@
+package de.ait.todo.services;
+
+import de.ait.todo.dto.NewTaskDto;
+import de.ait.todo.dto.TaskDto;
+import de.ait.todo.dto.TasksPage;
+
+/**
+ * 6/11/2023
+ * backend-demo
+ *
+ * @author Marsel Sidikov (AIT TR)
+ */
+public interface TasksService {
+    TasksPage getAll();
+
+    TaskDto getById(Long taskId);
+
+    void deleteTask(Long taskId);
+
+    TaskDto addTask(Long currentUserId, NewTaskDto task);
+}
